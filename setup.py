25e5e36419cd766322bedcfa8d580ba42ba19814
Matthew Munson <matthew.munson@uni-hamburg.de>
2018-07-04 15:31:29 +0200

    Bumped version and changed CHANGES.txt

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='HookTest',
-    version="1.2.0",
+    version="1.2.1",
     description='Hook Test Script for GitHub/CapiTainS repositories',
     long_description=long_description,
     url='http://github.com/Capitains/HookTest',
